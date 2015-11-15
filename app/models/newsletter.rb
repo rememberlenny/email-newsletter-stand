a35a31145b89a6c0abd6eb212f300acf9df2b164
rememberlenny <rememberlenny@gmail.com>
2015-11-15 18:18:21 -0500

    Save the ograph

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -1,2 +1,11 @@
 class Newsletter < ActiveRecord::Base
+
+  def self.get_ograph_image id
+    n = Newsletter.find id
+    og = OpenGraph.new(n.url)
+    if n.image_url.nil?
+      n.image_url = og.images.first
+      n.save
+    end
+  end
 end
