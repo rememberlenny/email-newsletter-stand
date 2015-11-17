309221bc45d57f24995a1682d7d519ef9cc733e1
rememberlenny <rememberlenny@gmail.com>
2015-11-17 09:36:40 -0500

    Create the backup

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,7 +2,11 @@ module PagesHelper
 
   def render_curated_row placement
     curation = Curation.where(placement: placement).first
-    curation_posts = CurationPosts.where(curation_id: curation.id)
+    if !curation.nil?
+      curation_posts = CurationPosts.where(curation_id: curation.id)
+    else
+      curation_posts = []
+    end
     return curation_posts
   end
 end
