6f37a185822db3bbce83dcc650287eebd289ac9c
rememberlenny <rememberlenny@gmail.com>
2015-11-24 14:05:24 -0500

    Save the check for admin

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,3 +1,26 @@
 class Email < ActiveRecord::Base
   validates :newsletter_id, presence: true
+
+  def self.check_for_welcome
+    subj = self.subject.downcase
+    flags = [
+      'for subscribing',
+      'welcome!',
+      'you for joining',
+      'thanks for signing',
+      'welcome to ',
+      'please confirm',
+      'subscription confirmed',
+      'welcome to your',
+      'email validation',
+      'confirm your',
+    ]
+    flags.each do |flag|
+      if subj.index(flag) != nil
+        self.admin_email = true
+        self.save
+      end
+    end
+  end
 end
+
