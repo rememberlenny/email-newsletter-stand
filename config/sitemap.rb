fb5d249d3301077db560e201232871cd7458fa58
rememberlenny <rememberlenny@gmail.com>
2015-11-28 15:51:09 -0500

    Test the sitemapps

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,4 +1,13 @@
-# Set the host name for URL creation
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(fog_provider: 'AWS',
+                                     aws_access_key_id: ENV['ENS_AWS_ACCESS_KEY_ID'],
+                                     aws_secret_access_key: ENV['ENS_AWS_SECRET_ACCESS_KEY'],
+                                     fog_directory: ENV['ENS_AWS_BUCKET'],
+                                     fog_region: ENV['ENS_AWS_REGION'])
+
+SitemapGenerator::Sitemap.public_path = 'tmp/'
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new
+SitemapGenerator::Sitemap.sitemaps_host = "http://#{ENV['ENS_AWS_BUCKET']}.s3.amazonaws.com/"
+SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 SitemapGenerator::Sitemap.default_host = "https://www.emailnewsletterstand.com"
 
 SitemapGenerator::Sitemap.create do
