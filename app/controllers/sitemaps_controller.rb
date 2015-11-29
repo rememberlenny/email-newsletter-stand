14dd138555b6dd478ace5b01c8e75421833e6395
rememberlenny <rememberlenny@gmail.com>
2015-11-28 23:36:24 -0500

    Fix ping

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -1,4 +1,7 @@
 class SitemapsController < ApplicationController
+  skip_authorization_check
+  skip_before_action :authenticate_user!
+
   def sitemap
     redirect_to "https://s3.amazonaws.com/#{ ENV['ENS_AWS_BUCKET'] }/sitemaps/sitemap.xml.gz"
   end
