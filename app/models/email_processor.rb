3b7bccde2e2f0fb50d6dda56a3ce4eb3ba424dc3
Lenny Bogdonoff <rememberlenny@gmail.com>
2015-12-01 14:03:01 -0500

    Save the email receiving

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,21 +1,27 @@
 class EmailProcessor
   def initialize(email)
+    # pry
     @email = email
     # pry
   end
 
   def process
+    # pry
     newsletter = Newsletter.find_by_email(@email.to[0][:token])
     if !newsletter.nil?
+    # pry
       Email.create(
         newsletter_id: newsletter.id,
         to: @email.to[0][:email],
         from: @email.from[:email],
+        subject: @email.subject,
         raw_text: @email.raw_text,
         raw_html: @email.raw_html,
         raw_body: @email.raw_body,
         body: @email.body
       )
+    # pry
     end
+    # pry
   end
 end
