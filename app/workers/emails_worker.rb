c349f60bf553b4e02c67dc4f46a491d3f80806e7
rememberlenny <rememberlenny@gmail.com>
2015-12-02 12:17:52 -0500

    Save the email

--- app/workers/emails_worker.rb
+++ app/workers/emails_worker.rb
@@ -11,30 +11,21 @@ class EmailsWorker
   # changes
   def work(raw_post)
     @email = JSON.parse(email)
+
+    opt = {}
+    @email["to"][0]["email"]  ? opt[:to]        = @email["to"][0]["email"] : ''
+    @email["from"]["email"]   ? opt[:from]      = @email["from"]["email"] : ''
+    @email["subject"]         ? opt[:subject]   = @email["subject"] : ''
+    @email["raw_text"]        ? opt[:raw_text]  = @email["raw_text"] : ''
+    @email["raw_html"]        ? opt[:raw_html]  = @email["raw_html"] : ''
+    @email["raw_body"]        ? opt[:raw_body]  = @email["raw_body"] : ''
+    @email["body"]            ? opt[:body]      = @email["body"] : ''
+
     newsletter = Newsletter.find_by_email(@email["to"][0]["token"])
     if !newsletter.nil?
-    # pry
-      Email.create(
-        newsletter_id: newsletter.id,
-        to: @email["to"][0]["email"],
-        from: @email["from"]["email"],
-        subject: @email["subject"],
-        raw_text: @email["raw_text"],
-        raw_html: @email["raw_html"],
-        raw_body: @email["raw_body"],
-        body: @email["body"]
-      )
-    # pry
+      Email.create(opt)
     else
-      UnknownEmail.create(
-        to: @email["to"][0]["email"],
-        from: @email["from"]["email"],
-        subject: @email["subject"],
-        raw_text: @email["raw_text"],
-        raw_html: @email["raw_html"],
-        raw_body: @email["raw_body"],
-        body: @email["body"]
-      )
+      UnknownEmail.create(opt)
     end
     ack! # we need to let queue know that message was received
   end
