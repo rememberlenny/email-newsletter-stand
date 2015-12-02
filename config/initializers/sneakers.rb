95a3eba18436b576e9aa3ec7889e95040a59820a
rememberlenny <rememberlenny@gmail.com>
2015-12-02 08:16:42 -0500

    Try the sneakers

--- config/initializers/sneakers.rb
+++ config/initializers/sneakers.rb
@@ -1,2 +1,8 @@
-Sneakers.configure({})
+opts = {
+  :amqp => ENV['RABBITMQ_BIGWIG_TX_URL'],
+  :exchange_type => :direct
+}
+
+Sneakers.configure(opts)
+
 Sneakers.logger.level = Logger::INFO # the default DEBUG is too noisy
