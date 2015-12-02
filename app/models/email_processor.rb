4a2bc2b5488d29d06567b0ae2ce27c5898406ea2
rememberlenny <rememberlenny@gmail.com>
2015-12-02 10:18:26 -0500

    Save the json interaction

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -3,19 +3,18 @@ class EmailProcessor
   def self.create_email(email)
     # pry
     @email = JSON.parse(email)
-    # pry
-    newsletter = Newsletter.find_by_email(@email.to[0][:token])
+    newsletter = Newsletter.find_by_email(@email["to"][0]["token"])
     if !newsletter.nil?
     # pry
       Email.create(
         newsletter_id: newsletter.id,
-        to: @email.to[0][:email],
-        from: @email.from[:email],
-        subject: @email.subject,
-        raw_text: @email.raw_text,
-        raw_html: @email.raw_html,
-        raw_body: @email.raw_body,
-        body: @email.body
+        to: @email["to"][0]["email"],
+        from: @email["from"]["email"],
+        subject: @email["subject"],
+        raw_text: @email["raw_text"],
+        raw_html: @email["raw_html"],
+        raw_body: @email["raw_body"],
+        body: @email["body"]
       )
     # pry
       ack! # we need to let queue know that message was received
