fae06fd4d01a7931695f5e818e6ffcf8a3ea35ea
rememberlenny <rememberlenny@gmail.com>
2015-12-02 19:16:51 -0500

    Save the email newsletter

--- app/services/sitemap.rb
+++ app/services/sitemap.rb
@@ -1,4 +1,4 @@
-class Sitemap < ActiveRecord::Base
+class Sitemap
   def self.refresh
     SitemapGenerator::Interpreter.run(config_file:'config/sitemap.rb')
     SitemapGenerator::Sitemap.ping_search_engines
