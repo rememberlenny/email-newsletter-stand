28e03453d2f64d248ca3786dad7e66de61565f5c
rememberlenny <rememberlenny@gmail.com>
2015-12-02 19:30:30 -0500

    SAve the sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -16,5 +16,23 @@ SitemapGenerator::Sitemap.create do
   Email.find_each do |email|
     add email_path(email), :lastmod => email.updated_at
   end
+end
+s3 = Aws::S3::Resource.new(
+  access_key_id: ENV['ENS_AWS_ACCESS_KEY_ID'],
+  secret_access_key: ENV['ENS_AWS_SECRET_ACCESS_KEY'],
+  region: ENV['ENS_AWS_REGION'],
+)
+Dir.entries(File.join(Rails.root, "public", "sitemaps")).each do |file_name|
+  next if ['.', '..'].include? file_name
+  path = "sitemaps/#{file_name}"
+  object = s3.bucket(ENV['ENS_AWS_BUCKET']).object(path)
+  file = File.join(Rails.root, "public", "sitemaps", file_name)
 
+  begin
+    object.upload_file(file)
+
+  rescue Exception => e
+    raise e
+  end
+  puts "Saved #{file_name} to S3"
 end
