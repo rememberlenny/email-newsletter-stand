b294dd4d8067b065405f55edf2965526db51c6c0
Lenny Bogdonoff <rememberlenny@gmail.com>
2015-12-04 11:00:04 -0500

    Save the unshortens

--- lib/unshorten.rb
+++ lib/unshorten.rb
@@ -1,11 +1,16 @@
 module Unshorten
   class << self
+
     def unshorten(url, options = {})
 		uri_str = URI.encode(url)
+		uri = URI.parse(url) rescue nil
+		return url if uri.nil?
 	    result = Curl::Easy.http_get(uri_str) do |curl|
-	      curl.follow_location = false
+			curl.follow_location = false
 	    end
-	    return result.header_str.split('Location: ')[1].split(' ')[0]
+	    if result && !result.nil? && result.header_str && !result.header_str.nil? && result.header_str.split('Location: ').count > 1
+			return result.header_str.split('Location: ')[1].split(' ')[0]
+		end
     end
     alias :'[]' :unshorten
   end
