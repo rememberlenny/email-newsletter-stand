b891828616f70f52dc09ca42c0960fb8ace74042
Lenny Bogdonoff <rememberlenny@gmail.com>
2015-12-04 11:06:59 -0500

    Fix the check for bad url

--- lib/unshorten.rb
+++ lib/unshorten.rb
@@ -2,6 +2,9 @@ module Unshorten
   class << self
 
     def unshorten(url, options = {})
+		httpcheck = url.split('http://')
+		httpscheck = url.split('https://')
+		return url if httpcheck.count == 0 || httpscheck.count == 0
 		uri_str = URI.encode(url)
 		uri = URI.parse(url) rescue nil
 		return url if uri.nil?
@@ -11,6 +14,7 @@ module Unshorten
 	    if result && !result.nil? && result.header_str && !result.header_str.nil? && result.header_str.split('Location: ').count > 1
 			return result.header_str.split('Location: ')[1].split(' ')[0]
 		end
+		return url 
     end
     alias :'[]' :unshorten
   end
